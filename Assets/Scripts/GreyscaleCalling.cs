c35a7b7c42234b4956948dfb9130b60d1ddb3623
Juli Bega <Julian.begamilrad@gmail.com>
2026-05-19 00:48:49 -0300

    Settup Greyscale Shader

---
+++ Assets/Scripts/GreyscaleCalling.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GreyscaleCalling : MonoBehaviour
+{
+    private Material _material;
+    public Shader _shader;
+
+    void Start() 
+    {
+        _material = new Material(_shader);
+    }
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _material);
+    }
+}
+
